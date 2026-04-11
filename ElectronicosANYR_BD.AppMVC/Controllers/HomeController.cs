e76096081b8ed5b90403291fb0dd70bccefb1c46
REINA <reinadejesuslue@gmail.com>
2026-04-11 15:59:50 -0600

    Envió cambios del proyecto Reina hice cambios en contrlador home y vista index

--- ElectronicosANYR_BD.AppMVC/Controllers/HomeController.cs
+++ ElectronicosANYR_BD.AppMVC/Controllers/HomeController.cs
@@ -1,25 +1,49 @@
 using System.Diagnostics;
+
 using ElectronicosANYR_BD.AppMVC.Models;
+
 using Microsoft.AspNetCore.Mvc;
 
+
+
 namespace ElectronicosANYR_BD.AppMVC.Controllers
+
 {
+
     public class HomeController : Controller
+
     {
+
         public IActionResult Index()
+
         {
+
             return View();
+
         }
+
+
 
         public IActionResult Privacy()
+
         {
+
             return View();
+
         }
 
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+
         public IActionResult Error()
+
         {
+
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+
         }
+
     }
+
 }
